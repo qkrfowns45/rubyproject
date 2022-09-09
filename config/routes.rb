381b37548b2cffa26d048ee718d1ca5abf54b5a8
qkrfowns45 <qkrfowns45@naver.com>
2022-09-09 07:21:09 +0000

    board add but not complete

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
     devise_for :users
   root "posts#index"
   resources :posts
+  get '/posts/board', to: 'posts#board'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
